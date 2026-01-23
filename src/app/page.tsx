2e58ae9686788d8c51adce3cb7aeafad5e6aebba
Andre Luiz Polzin <andrelpolzin@gmail.com>
2026-01-23 10:15:58 -0300

    Added Dialog so now we have a better modal for editing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,27 @@ import {
   SquarePen,
   Trash2,
   ListCheck,
+  Sigma,
 } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const Home = () => {
   return (
@@ -53,21 +72,69 @@ const Home = () => {
               <div className="w-1 h-full bg-green-300"></div>
               <p className="flex-1 px-2 text-sm">Study ReactJs</p>
               <div className="flex items-center gap-2">
-                <SquarePen size={18} className="cursor-pointer" />
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <SquarePen size={18} className="cursor-pointer" />
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>Edit task</DialogTitle>
+                    </DialogHeader>
+
+                    <div className="flex gap-2">
+                      <Input placeholder="Edit task..." />
+                      <Button className="cursor-pointer">Edit</Button>
+                    </div>
+                  </DialogContent>
+                </Dialog>
                 <Trash2 size={18} className="cursor-pointer" />
               </div>
             </div>
           </div>
         </CardContent>
 
-        <CardFooter>
-          <div className="flex items-center justify-between">
-            <ListCheck />
-            <p>Completed Tasks</p>
-            <p className="flex">
-              <Trash2 />
-              Clear completed tasks
-            </p>
+        <CardFooter className="flex flex-col">
+          <div className="flex items-center justify-between w-full">
+            <div className="flex items-center gap-1">
+              <ListCheck size={18} />
+              <p className="text-xs">Completed Tasks (3/3)</p>
+            </div>
+
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button
+                  className="flex rounded-lg justify-center items-center p-1 gap-2 cursor-pointer text-xs h-7"
+                  variant={"outline"}
+                >
+                  <Trash2 size={18} />
+                  Clear completed tasks
+                </Button>
+              </AlertDialogTrigger>
+
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>
+                    Are you sure you want to delete x items?
+                  </AlertDialogTitle>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogAction>Yes</AlertDialogAction>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </div>
+
+          <div className="h-2 bg-gray-100 w-full mt-4 rounded-md">
+            <div
+              className="h-full bg-blue-500 rounded-md"
+              style={{ width: "50%" }}
+            ></div>
+          </div>
+
+          <div className="flex gap-2 justify-end w-full mt-2">
+            <Sigma size={18} />
+            <p className="text-xs">3 tasks in total</p>
           </div>
         </CardFooter>
       </Card>
